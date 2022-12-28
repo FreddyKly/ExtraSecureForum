b6a163cf443761238c85d506fb27f6fb16b7c3ba
FreddyKly <Frederik.Kliemt@web.de>
2022-12-28 20:53:51 +0100

    router init

--- Frontend/secure-frontend/src/main.js
+++ Frontend/secure-frontend/src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  },
+})
+
+createApp(App).use(vuetify).use(router).mount('#app')

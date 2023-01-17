53d463f6219e412b23f5e3b0efd982b8fc85e8c5
FreddyKly <Frederik.Kliemt@web.de>
2023-01-17 20:55:30 +0100

    update new apis

--- Backend/server.js
+++ Backend/server.js
@@ -11,16 +11,20 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 const users = require('./API/users');
-const posts = require('./API/posts');
+const threads = require('./API/threads');
+const answers = require('./API/answers');
 
 app.use('/api/users', users);
-app.use('/api/posts', posts);
+app.use('/api/threads', threads);
+app.use('/api/answers', answers);
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
+app.use(express.json({limit: "10mb", extended: true}))
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({limit: "10mb", extended: true, parameterLimit: 50000}))
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;

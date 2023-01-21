7c325070e1fabfd44e2c0c4d9ea92c12d5db9223
Henry <henrykniss@googlemail.com>
2023-01-21 18:00:00 +0100

    CAn post and display anwsers including HTML

--- Frontend/src/components/aNewSide.vue
+++ Frontend/src/components/aNewSide.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card class="mx-auto" max-width="344" outlined>
+  <v-card class="mx-auto" max-width="900" outlined>
     <v-list-item three-line>
       <v-list-item-content>
         <div v-text="this.thread.title" class="text-overline mb-4">
@@ -23,7 +23,7 @@
           <v-textarea v-model="newPost" filled label="Your Text here" auto-grow>
           </v-textarea>
           <v-card-actions>
-            <v-btn outlined rounded text @click="savePost">
+            <v-btn outlined rounded text @click="savePost(); this.$router.go()">
               Confirm
             </v-btn>
           </v-card-actions>
@@ -32,9 +32,24 @@
 
       <div class="mx-4 hidden-sm-and-down"></div>
     </v-card-actions>
+    <v-container v-for="(post, n) in listofAnwsers" :key="n" class="grey lighten-5 mb-6">
+      <v-row :align="align" no-gutters style="height: 200px;">
+        <v-col v-for="y in 1" :key="y">
+          <v-card class="mx-auto" max-width="900" outlined>
+            <v-list-item three-line>
+              <v-list-item-content>
+                <v-list-item-subtitle v-html="post.post"></v-list-item-subtitle>
+              </v-list-item-content>
 
-    
+              <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+            </v-list-item>
 
+            <v-card-actions>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
 
@@ -43,8 +58,8 @@ import postService from "@/services/postService";
 import threadService from "@/services/threadService.js"
 export default {
   async created() {
+    this.threadId = window.location.pathname.split('/')[2];
     try {
-      this.threadId = window.location.pathname.split('/')[2];
 
       this.listOfPosts = await threadService.getThread(this.threadId);
       this.listOfPosts.concat(this.expand);
@@ -56,24 +71,33 @@ export default {
     } catch (error) {
       console.log(error);
     }
+
+    try {
+      this.listofAnwsers= await postService.getAnwsers(this.threadId);
+      console.log(this.listofAnwsers);
+
+    } catch (error) {
+      console.log(error);
+    }
   },
 
 
   data: () => ({
-    threadId:"",
-    contentToPost:"",
+    listofAnwsers:[],
+    threadId: "",
+    contentToPost: "",
     expand: false,
     listOfPosts: [],
     thread: [],
   }),
 
-  mounted(){
+  mounted() {
     this.contentToPost = this.newPost;
   },
-  methods:{
-    savePost(){
+  methods: {
+    savePost() {
       console.log(this.newPost);
-      postService.postAwnser({"thread_id":this.threadId, "post":this.newPost})
+      postService.postAwnser({ "thread_id": this.threadId, "post": this.newPost })
 
 
     }
--- Frontend/src/services/postService.js
+++ Frontend/src/services/postService.js
@@ -2,6 +2,21 @@ import axios from 'axios';
 const url = 'http://localhost:8080/api/answers/';
 
 class postService {
+
+    static getAnwsers(threadId) {
+        // eslint-disable-next-line no-async-promise-executor
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await axios.get(url+threadId);
+                const data = res.data;
+                resolve(
+                    data);
+            } catch (err) {
+                reject(err);
+            }
+        })
+    }
+
     // Get detections
     static postAwnser(postObject) {
         // eslint-disable-next-line no-async-promise-executor

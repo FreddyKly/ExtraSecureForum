cfe23f33f994f49be72e88c3a2bc3694b8c77a01
FreddyKly <Frederik.Kliemt@web.de>
2023-01-24 15:32:20 +0100

    use sessions

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const session = require('express-session');
 
 const app = express();
 
@@ -10,6 +11,13 @@ var corsOptions = {
 
 // Middleware - Has to be placed infront of the routes
 app.use(cors(corsOptions));
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
 // parse requests of content-type - application/json
 app.use(express.json({limit: "10mb", extended: true}))
 

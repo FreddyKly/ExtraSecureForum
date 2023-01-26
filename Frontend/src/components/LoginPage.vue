e6b82977083d580bbefc15f4c4d809f237eb70fe
FreddyKly <Frederik.Kliemt@web.de>
2023-01-26 02:46:28 +0100

    fixed login page

--- Frontend/src/components/LoginPage.vue
+++ Frontend/src/components/LoginPage.vue
@@ -1,30 +1,40 @@
 <template>
+  <v-app>
   <v-form>
     <v-container fluid>
       <v-row>
         <v-col>
-          <v-text-field v-model="loginAccountname" name="input-10-1" @click:append="show1 = !show1"></v-text-field>
-
-          <v-text-field v-model="loginPassword" :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
-            :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" counter
-            @click:append="show2 = !show2"></v-text-field>
+          <v-text-field
+            prepend-icon="mdi-account"
+            name="login"
+            label="Login"
+            type="text"
+          ></v-text-field>
+          <v-text-field
+            id="password"
+            prepend-icon="mdi-lock"
+            name="password"
+            label="Password"
+            :append-icon="password ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="() => (password = !password)"
+            :type="password ? 'password' : 'text'"
+          ></v-text-field>
         </v-col>
-        <v-btn outlined rounded text @click="Login();">
+        <v-btn rounded @click="Login();">
           Login
         </v-btn>
 
       </v-row>
     </v-container>
   </v-form>
+</v-app>
 </template>
 <script>
 import userService from "@/services/userService.js";
 export default {
   data() {
     return {
-      show1: false,
       accountname: "",
-      show2: false,
       password: 'Password',
       rules: {
         emailMatch: () => (`The email and password you entered don't match`),

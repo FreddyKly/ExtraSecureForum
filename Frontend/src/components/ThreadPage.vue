fc9eed094df275fb57c51875eec1ccf80b4f772d
FreddyKly <Frederik.Kliemt@web.de>
2023-01-31 23:43:27 +0100

    thread title margin

--- Frontend/src/components/ThreadPage.vue
+++ Frontend/src/components/ThreadPage.vue
@@ -14,7 +14,7 @@
       <v-list-item three-line>
 
         <v-list-item-content>
-          <div v-text="this.thread.title" class="text-h4 mb-4">
+          <div v-text="this.thread.title" class="text-h4 mb-4 mt-4">
 
           </div>
           <v-list-item-title class="text-h5 mb-1">
